1233d6cb6a7977ea94875c5b3ac4452986e28efb
Olga Borovyk <pennerall@gmail.com>
2016-08-02 00:26:37 +0300

    creating makeCopy function

--- src/main/java/system/dscope/Main.java
+++ src/main/java/system/dscope/Main.java
@@ -1,5 +1,7 @@
 package system.dscope;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,7 +10,10 @@ public class Main
 {
     public static void main( String[] args )
     {
-
-        System.out.println( "Hello World!" );
+        MediaFile image = new MediaFile("src/main/resources/Firstname, Lastname 01-08-2016 21-10-00 123451.jpg", "Firstname, Lastname 01-08-2016 21-10-00 123451.jpg");
+        System.out.println(image.getImg().getAbsolutePath());
+        System.out.println(image.getImg().exists());
+        image.makeCopy(image, 20);
     }
+
 }
--- src/main/java/system/dscope/MediaFile.java
+++ src/main/java/system/dscope/MediaFile.java
@@ -1,9 +1,31 @@
 package system.dscope;
 
+import java.io.File;
+
 /**
  * Created by Olga on 01.08.2016.
  */
 public class MediaFile {
-    
+    private File img;
+    private String fileName;
 
+    public MediaFile(String img, String fileName) {
+        this.img = new File(img);
+        this.fileName = fileName;
+    }
+
+    public File getImg() {
+        return img;
+    }
+
+    public void setImg(File img) {
+        this.img = img;
+    }
+
+    public void makeCopy(MediaFile img, int count) {
+        for (int i = 00; i < count; i++) {
+       //     MediaFile copy = new MediaFile();
+
+        }
+    }
 }
