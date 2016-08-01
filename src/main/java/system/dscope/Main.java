9f5234711c9d86c069c3e10d75de86682017ceef
Olga Borovyk <pennerall@gmail.com>
2016-08-01 23:35:01 +0300

    create project

---
+++ src/main/java/system/dscope/Main.java
@@ -0,0 +1,14 @@
+package system.dscope;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/system/dscope/MediaFile.java
@@ -0,0 +1,9 @@
+package system.dscope;
+
+/**
+ * Created by Olga on 01.08.2016.
+ */
+public class MediaFile {
+    
+
+}
