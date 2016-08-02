aaab13c14e77a3ed33281dfbebfad4fe5fb666ba
Olga Borovyk <pennerall@gmail.com>
2016-08-02 11:22:32 +0300

    try read and write file

--- src/main/java/system/dscope/Main.java
+++ src/main/java/system/dscope/Main.java
@@ -1,7 +1,5 @@
 package system.dscope;
 
-import java.io.File;
-
 /**
  * Hello world!
  *
@@ -11,8 +9,8 @@ public class Main
     public static void main( String[] args )
     {
         MediaFile image = new MediaFile("src/main/resources/Firstname, Lastname 01-08-2016 21-10-00 123451.jpg", "Firstname, Lastname 01-08-2016 21-10-00 123451.jpg");
-        System.out.println(image.getImg().getAbsolutePath());
-        System.out.println(image.getImg().exists());
+        System.out.println(image.getPass().getAbsolutePath());
+        System.out.println(image.getPass().exists());
         image.makeCopy(image, 20);
     }
 
--- src/main/java/system/dscope/MediaFile.java
+++ src/main/java/system/dscope/MediaFile.java
@@ -1,6 +1,6 @@
 package system.dscope;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by Olga on 01.08.2016.
@@ -9,23 +9,40 @@ public class MediaFile {
     private File img;
     private String fileName;
 
-    public MediaFile(String img, String fileName) {
-        this.img = new File(img);
+    public MediaFile(String path, String fileName) {
+        this.img = new File(path);
         this.fileName = fileName;
     }
 
-    public File getImg() {
+    public MediaFile() {
+
+    }
+
+    public File getPath() {
         return img;
     }
 
-    public void setImg(File img) {
+    public void setPath(File img) {
         this.img = img;
     }
 
-    public void makeCopy(MediaFile img, int count) {
-        for (int i = 00; i < count; i++) {
-       //     MediaFile copy = new MediaFile();
+    public void makeCopy(MediaFile img, int count) throws FileNotFoundException {
+//        InputStream inputStream = new FileInputStream(img.getPass());
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(img.getPath())));
+        }
+        catch (FileNotFoundException e1) {
+            System.out.println("File not found");
+            System.exit(0);
+        }
 
+        for (int i = 00; i < count; i++) {
+            MediaFile copy = new MediaFile();
+            copy.setPath(img.getPath());
+
+            FileOutputStream outputStream = new FileOutputStream("");
         }
     }
 }
